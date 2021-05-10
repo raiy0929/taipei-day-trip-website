018ed030970e916db6afa97c354d63565e5f348a
Raiy <raiybow@gmail.com>
2021-05-10 11:49:19 +0800

    fix flask multi threads

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import *
 import pymysql
 import pymysql.cursors
-import os, json, traceback, config
+import os, json, traceback, config, threading
 import pymysqlpool
 
 
@@ -16,9 +16,11 @@ REMOTE_PASSWORD=app.config["REMOTE_DB_PASSWORD"]
 
 app.secret_key=os.urandom(12).hex()
 
-db=pymysql.connect(host="127.0.0.1",user=USER,password=PASSWORD,database="TravelWeb")
-# db=pymysql.connect(host="127.0.0.1",user=REMOTE_USER,password=REMOTE_PASSWORD,database="TravelWeb")
+# db=pymysql.connect(host="127.0.0.1",user=USER,password=PASSWORD,database="TravelWeb")
+db=pymysql.connect(host="127.0.0.1",user=REMOTE_USER,password=REMOTE_PASSWORD,database="TravelWeb")
 cur=db.cursor()
+
+lock = threading.Lock()
 
 '''確認資料庫連線'''
 def check_connection():
@@ -26,7 +28,9 @@ def check_connection():
     sql="select * from attractions where id = 1"
     while True:
         try:
+            lock.acquire()
             cur.execute(sql)
+            lock.release()
             break
             # return cur, db
         except pymysql.err.InterfaceError as e :
@@ -49,9 +53,10 @@ def booking():
 def thankyou():
     return render_template("thankyou.html")
 
-# 練習 會員頁
+# member page
 @app.route("/member")
 def member():
+    
     return render_template("member.html")
 
 
@@ -67,12 +72,14 @@ def getAttractions():
         if request.args.get('keyword') != None: 
             keyword = request.args.get('keyword')
             # 取12個景點
-            
+            lock.acquire()
             cur.execute(f'select * from attractions where title like "%{keyword}%" order by attrId limit {landCount},12') 
+            lock.release()
             result=cur.fetchall()
 
-            
+            lock.acquire()
             cur.execute(f'select * from attractions where title like "%{keyword}%"')
+            lock.release()
             count=cur.fetchall()
 
             if len(result) == 0: # 該keyword無符合景點
@@ -110,10 +117,14 @@ def getAttractions():
                 return jsonify(allLand)
 
         else: # 沒keyword 傳所有資料
+            lock.acquire()
             cur.execute(f'select * from attractions order by attrId limit {landCount}, 12') 
+            lock.release()
             result=cur.fetchall()
 
+            lock.acquire()
             cur.execute('select * from attractions')
+            lock.release()
             count=cur.fetchall()
 
             if len(count)-landCount > 12:  # 頁數
@@ -156,7 +167,9 @@ def getAttractions():
 def idGetAttr(attractionId):
     try:
         attractionId = int(attractionId)
+        lock.acquire()
         cur.execute(f'select * from attractions where attrID = {attractionId}')
+        lock.release()
         result=cur.fetchone()
         if result != None: #有該景點
             landPhoto=eval(result[5]) #處理景點圖片
@@ -202,14 +215,20 @@ def user():
             if email == "" or name == "" or password == "":
                 return jsonify({"error": True, "message": "輸入框不可為空"}), 400
             else:
+                lock.acquire()
                 cur.execute(f'select * from member where email = "{email}"')
+                lock.release()
                 result = cur.fetchone()
                 if result != None:
                     return jsonify({"error": True, "message": "email已被使用"}),400
                 else :
+                    lock.acquire()
                     cur.execute(f'Insert into member (name, email, password) VALUES ("{name}", "{email}", "{password}")')
+                    lock.release()
                     db.commit()
+                    lock.acquire()
                     cur.execute(f'select * from member where email = "{email}"')
+                    lock.release()
                     check_register = cur.fetchone()
                     if check_register != None:
                         return jsonify({"ok":True}),200
@@ -220,10 +239,14 @@ def user():
             data = request.get_json()
             email = data["email"]
             password = data["password"]
+            lock.acquire()
             cur.execute(f'select * from member where email = "{email}"')
+            lock.release()
             result = cur.fetchone()
             if  password == result[3]:
-                cur.execute(f'update member set `logining` = 1 where email = "{email}"') #  change DB login status
+                lock.acquire()
+                cur.execute(f'update member set `logining` = 1 where email = "{email}"')
+                lock.release() #  change DB login status
                 db.commit()
                 resp = jsonify({"ok": True})
                 resp.set_cookie('user', email, max_age=86400)
@@ -236,7 +259,9 @@ def user():
         elif request.method == "GET": # Get user profile auto fetch
             email = request.cookies.get("user")
             if email != None:
+                lock.acquire()
                 cur.execute(f'select * from member where email = "{email}" ')
+                lock.release()
                 result = cur.fetchone()
                 memberId = result[0]
                 name = result[1]
@@ -252,11 +277,15 @@ def user():
         
         elif request.method == "DELETE": # login out
             email = request.cookies.get("user")
+            lock.acquire()
             cur.execute(f'update member set logining = 2 where email = "{email}"')
+            lock.release()
             db.commit()
             resp = jsonify({"ok":True})
             resp.delete_cookie("user")
+            lock.acquire()
             cur.execute(f'select * from member where email = "{email}"')
+            lock.release()
             result = cur.fetchone()
             return resp
             
